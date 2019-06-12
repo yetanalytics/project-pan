656d9fec84fc9d6a62161046bb391a913f7f737c
Kelvin Qian <kelvin@Yets-MacBook-Pro.local>
2019-06-12 13:24:55 -0400

    Init commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to project-pan
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/com/yetanalytics/project_pan_test.clj
@@ -0,0 +1,7 @@
+(ns com.yetanalytics.project-pan-test
+  (:require [clojure.test :refer :all]
+            [com.yetanalytics.project-pan :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
