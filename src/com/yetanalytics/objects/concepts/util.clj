5a842fc41854e62d227943b2d48d7e3db6925c78
William Hoyt <will@yetanalytics.com>
2019-06-24 15:04:54 -0400

    IMPORTANT NOTES from Kelvin + Will call - thinking top-down

--- src/com/yetanalytics/objects/concepts/util.clj
+++ src/com/yetanalytics/objects/concepts/util.clj
@@ -31,6 +31,94 @@
                                               :?deprecated ?deprecated})]
     (u/containsv? concepts iri)))
 
+(comment
+  ;; Example of how to do more effecient
+  (def dummy-profile {:concepts [{:id "https://www.some-target-iri.com/miss"
+                                  :type "ActivityType"}
+                                 {:id "https://www.some-target-iri.com"
+                                  :type "Verb"}
+                                 {:id "https://www.some-target-iri.com/another-hit"
+                                  :type "Verb"}]
+                      :templates []
+                      :patterns []})
+
+  (let [valid-targets (ids-of-target-concept-type {:target-type-str "Verb"
+                                                   :profile dummy-profile})]
+    (mapv (fn [iri] (u/containsv? valid-targets iri)) ["https://www.some-target-iri.com" "https://www.some-target-iri.com/another-hit"]))
+
+  (iri-in-profile-concepts?
+   {:iri "https://www.some-target-iri.com"
+    :target-type-str "Verb"
+    :profile dummy-profile})
+
+  (iri-in-profile-concepts?
+   {:iri "https://www.some-target-iri.com/another-hit"
+    :target-type-str "Verb"
+    :profile dummy-profile})
+
+  ;; example of how to do strict validation at top level
+  (s/valid? ::stict-profile-spec profile)
+  (s/def ::strict-profile-spec
+    (fn [{:keys [concepts patterns templates]}]
+      (s/valid? ::strict-concepts concepts)
+      (s/valid? ::strict-patterns patterns)
+      (s/valid? ::strict-templates templates)))
+
+  (defn get-external-thing
+    [id]
+    "got the thing")
+
+  (s/def ::dependent-on-external
+    (fn [m]
+      (let [{thing-to-check :check-me
+             to-check-against :resolve-me} m
+            data (get-external-thing to-check-against)]
+        ;; focus on the predicate instead of the external resolution
+        (= thing-to-check data))))
+
+  (defn validate-profile
+    [profile]
+    (reduce-kv
+     (fn [accum k v]
+       (let [{profile-id :id} profile]
+        (case k
+         :id accum ;; no op bc nothing to be more strict about
+         :concepts (for [each v] ;; v = concepts in the profile
+                     (let [{concept-type :type} each]
+                       (reduce-kv ;; think from this persepctive
+                        (fn [ack concept-k concept-v]
+                          ;; TODO: stepping in pattern matching territory
+                          ;; - core.match vs meander comes in
+                          (case (and concept-k concept-type)
+                            :another-example-key
+                            (conj ack (s/valid? :something-needs-val-from-profile
+                                                {:value-to-check     concept-v
+                                                 :value-from-profile profile-id}))
+                            :example-key-with-additional-constraints
+                            (conj ack (s/valid? ::u/in-scheme-strict-scalar
+                                                {:in-scheme concept-v
+                                                 :profile profile}))
+                            "...")) [] each)))
+         :templates "..."
+         :patterns "..."
+         "..."))) [] profile))
+
+  {:concept-1 [true true {:SPEC-ERROR "some data"} true {:SPEC-ERROR "some data"}]
+   :concept-2 [true true {:SPEC-ERROR "some data"}]}
+
+  (validate-profile dummy-profile)
+
+  (reduce-kv
+   (fn [accum k v]
+     (if (= k :foo)
+       (conj accum v)
+       (conj accum (:qux v))))
+   []
+   {:foo "bar" :baz {:qux "hello world"}})
+  )
+
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; specs
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
