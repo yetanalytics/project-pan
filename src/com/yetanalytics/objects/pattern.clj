a11a93c4b1e048ac90cd1b70710ad802d448c02f
William Hoyt <will@yetanalytics.com>
2019-06-24 02:42:12 -0400

    refactored patterns

--- src/com/yetanalytics/objects/pattern.clj
+++ src/com/yetanalytics/objects/pattern.clj
@@ -1,69 +1,56 @@
 (ns com.yetanalytics.objects.pattern
   (:require [clojure.spec.alpha :as s]
-            [xapi-schema.spec :as xs]
-            [com.yetanalytics.axioms :as ax]
-            [com.yetanalytics.objects.object :as object]))
+            [com.yetanalytics.axioms :as ax]))
 
 ;; Basic properties
 
 (s/def ::id ::ax/uri)
 (s/def ::type #{"Pattern"})
 (s/def ::primary ::ax/boolean)
+(s/def ::in-scheme ::ax/iri)
+(s/def ::pref-label ::ax/language-map)
+(s/def ::definition ::ax/language-map)
+(s/def ::deprecated ::ax/boolean)
 
-;; Regex properties
-(s/def :regex/id ::ax/iri)
-
-(s/def ::alternates
-  (s/coll-of ::ax/iri :type vector? :min-count 2))
-(s/def ::sequence
-  (s/coll-of ::ax/iri :type vector? :min-count 1))
-(s/def ::optional
-  (s/and (s/keys :req-un [:regex/id]) (xs/restrict-keys :id)))
-(s/def ::one-or-more
-  (s/and (s/keys :req-un [:regex/id]) (xs/restrict-keys :id)))
-(s/def ::zero-or-more
-  (s/and (s/keys :req-un [:regex/id]) (xs/restrict-keys :id)))
-
-(s/def ::common
-  (s/keys :req-un [(or ::alternates
-                       ::optional
-                       ::one-or-more
-                       ::sequence
-                       ::zero-or-more)]
-          :opt-un [::primary
-                   ::object/in-scheme
-                   ::object/deprecated]))
-
-;; Descriptions (prefLabel and definition) are mandatory for primary patterns
-;; and are optional for non-primary patterns.
-
-(defmulti pattern? ::primary)
-
-(defmethod pattern? true [_]
-  (s/merge ::common ::object/description))
-
-(defmethod pattern? :default [_]
-  (s/merge ::common
-           (s/keys :opt-un [:object/pref-label
-                            :object/definition])))
+(s/def ::alternates (s/coll-of ::ax/iri :type vector? :min-count 2))
+(s/def ::optional (s/keys :req-un [::id]))
+(s/def ::one-or-more (s/keys :req-un [::id]))
+(s/def ::sequence (s/coll-of ::ax/iri :type vector? :min-count 1))
+(s/def ::zero-or-more (s/keys :req-un [::id]))
 
 ;; Ensure that only one of the five regex properties are included in pattern.
 ;; Including two or more properties should fail the spec.
 
-(defn pattern-xor? [p]
-  (let [alt? (contains? p :alternates)
-        opt? (contains? p :optional)
-        oom? (contains? p :one-or-more)
-        sqn? (contains? p :sequence)
-        zom? (contains? p :zero-or-more)]
-    (cond
-      alt? (not (or opt? oom? sqn? zom?))
-      opt? (not (or alt? oom? sqn? zom?))
-      oom? (not (or alt? opt? sqn? zom?))
-      sqn? (not (or alt? opt? oom? zom?))
-      zom? (not (or alt? opt? oom? sqn?)))))
+(s/def ::pattern-clause
+  (fn [p]
+    (let [alt? (contains? p :alternates)
+          opt? (contains? p :optional)
+          oom? (contains? p :one-or-more)
+          sqn? (contains? p :sequence)
+          zom? (contains? p :zero-or-more)]
+      (cond
+        alt? (not (or opt? oom? sqn? zom?))
+        opt? (not (or alt? oom? sqn? zom?))
+        oom? (not (or alt? opt? sqn? zom?))
+        sqn? (not (or alt? opt? oom? zom?))
+        zom? (not (or alt? opt? oom? sqn?))))))
 
-(s/def ::pattern (s/and (s/multi-spec pattern? ::primary)
-                        pattern-xor?))
+(s/def ::primary-pattern
+  (s/and (s/keys :req-un [::id ::type ::pref-label ::definition ::primary]
+                 :opt-un [::in-scheme ::deprecated ::alternates ::optional
+                          ::one-or-more ::sequence ::zero-or-more])
+         ::pattern-clause))
 
-(s/def ::patterns (s/coll-of ::pattern :kind vector? :min-count 1))
+(s/def ::pattern
+  (s/and (s/keys :req-un [::id ::type]
+                 :opt-un [::primary ::in-scheme ::pref-label ::definition
+                          ::deprecated ::alternates ::optional ::one-or-more
+                          ::sequence ::zero-or-more])
+         ::pattern-clause))
+
+(s/def ::patterns
+  (s/coll-of (s/or :pattern ::pattern
+                   :primary ::primary-pattern) :kind vector? :min-count 1))
+
+;; TODO: MUST + MUST NOTS from Profile Authors: section
+;; https://github.com/adlnet/xapi-profiles/blob/master/xapi-profiles-structure.md#90-patterns
