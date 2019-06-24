693a5a3ae5585cd047534f472dff31d813c24bc8
William Hoyt <will@yetanalytics.com>
2019-06-24 00:44:42 -0400

    added common validation specs to util ns

--- src/com/yetanalytics/util.clj
+++ src/com/yetanalytics/util.clj
@@ -1,8 +1,9 @@
-(ns com.yetanalytics.util)
+(ns com.yetanalytics.util
+  (:require [clojure.spec.alpha :as s]))
 
-(defn containsv? [coll v]
-  (try (not= -1 (.indexOf coll v))
-       (catch Exception e false)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; fns + specs
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn not-empty? [coll] (coll? (not-empty coll)))
 
@@ -18,12 +19,25 @@
       true ;; we have no falses and some true
       false))) ;; we may have nils
 
+(s/def ::valid-boolean-coll
+  (fn [coll] valid-boolean-cool? coll))
+
 (defn only-ids [filtered-coll] (mapv (fn [{:keys [id]}] id) filtered-coll))
+
+(defn filter-by-in-scheme
+  [target-in-scheme coll]
+  (filterv (fn [{:keys [in-scheme]}] (= target-in-scheme in-scheme)) coll))
+
+(defn containsv? [coll v]
+  (try (not= -1 (.indexOf coll v))
+       (catch Exception e false)))
 
 (defn in-scheme?
   [in-scheme profile]
   (-> profile :versions only-ids (containsv? in-scheme)))
 
-(defn filter-by-in-scheme
-  [target-in-scheme coll]
-  (filterv (fn [{:keys [in-scheme]}] (= target-in-scheme in-scheme)) coll))
+(s/def ::in-scheme-strict-scalar
+  (fn [{:keys [in-scheme profile]}]
+    (in-scheme? in-scheme profile)))
+
+
