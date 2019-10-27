77ecffc22874b5437f10553a11b4f3e7767937d0
William Hoyt <will@yetanalytics.com>
2019-10-27 14:45:23 -0400

    convention update

--- test/com/yetanalytics/pan_test.clj
+++ test/com/yetanalytics/pan_test.clj
@@ -14,29 +14,29 @@
   (testing "Tests on Will's CATCH profile, unfixed"
     (is (not= (with-out-str (validate-profile will-profile-raw))
               "Success!\n"))
-    (is (not= (with-out-str (validate-profile will-profile-raw :syntax false :ids true))
+    (is (not= (with-out-str (validate-profile will-profile-raw :syntax? false :ids? true))
               "Success!\n"))
-    (is (not= (with-out-str (validate-profile will-profile-raw :syntax false :relations true))
+    (is (not= (with-out-str (validate-profile will-profile-raw :syntax? false :relations? true))
               "Success!\n"))
-    (is (= (with-out-str (validate-profile will-profile-raw :syntax false :contexts true))
+    (is (= (with-out-str (validate-profile will-profile-raw :syntax? false :contexts? true))
            "Success!\n"))
     ;; Testing print-errs being off
-    (is (some? (validate-profile will-profile-raw :print-errs false)))
-    (is (contains? (validate-profile will-profile-raw :print-errs false)
+    (is (some? (validate-profile will-profile-raw :print-errs? false)))
+    (is (contains? (validate-profile will-profile-raw :print-errs? false)
                    :syntax-errors))
-    (is (nil? (validate-profile will-profile-raw :syntax false :contexts true
-                                :print-errs false)))))
+    (is (nil? (validate-profile will-profile-raw :syntax? false :contexts? true
+                                :print-errs? false)))))
 
 ;; Fixed profile
 (deftest will-fix-test
   (testing "Tests on Will's CATCH profile, fixed"
     (is (= (with-out-str (validate-profile will-profile-fix))
            "Success!\n"))
-    (is (= (with-out-str (validate-profile will-profile-fix :ids true))
+    (is (= (with-out-str (validate-profile will-profile-fix :ids? true))
            "Success!\n"))
-    (is (= (with-out-str (validate-profile will-profile-fix :relations true))
+    (is (= (with-out-str (validate-profile will-profile-fix :relations? true))
            "Success!\n"))
-    (is (= (with-out-str (validate-profile will-profile-fix :contexts true))
+    (is (= (with-out-str (validate-profile will-profile-fix :contexts? true))
            "Success!\n"))))
 
 ;; Raw profile
@@ -44,11 +44,11 @@
   (testing "Tests on the cmi5 profile"
     (is (not= (with-out-str (validate-profile cmi-profile-raw))
               "Success!\n"))
-    (is (not= (with-out-str (validate-profile cmi-profile-raw :syntax false :ids true))
+    (is (not= (with-out-str (validate-profile cmi-profile-raw :syntax? false :ids? true))
               "Success!\n"))
-    (is (not= (with-out-str (validate-profile cmi-profile-raw :syntax false :relations true))
+    (is (not= (with-out-str (validate-profile cmi-profile-raw :syntax? false :relations? true))
               "Success!\n"))
-    (is (= (with-out-str (validate-profile cmi-profile-raw :syntax false :contexts true))
+    (is (= (with-out-str (validate-profile cmi-profile-raw :syntax? false :contexts? true))
            "Success!\n"))))
 
 ;; Fixed profile
@@ -56,9 +56,9 @@
   (testing "Tests on the cmi5 profile after it has been fixed"
     (is (= (with-out-str (validate-profile cmi-profile-fix))
            "Success!\n"))
-    (is (= (with-out-str (validate-profile cmi-profile-fix :syntax false :ids true))
+    (is (= (with-out-str (validate-profile cmi-profile-fix :syntax? false :ids? true))
            "Success!\n"))
-    (is (not= (with-out-str (validate-profile cmi-profile-fix :syntax false :relations true))
+    (is (not= (with-out-str (validate-profile cmi-profile-fix :syntax? false :relations? true))
               "Success!\n"))
-    (is (= (with-out-str (validate-profile cmi-profile-fix :syntax false :contexts true))
+    (is (= (with-out-str (validate-profile cmi-profile-fix :syntax? false :contexts? true))
            "Success!\n"))))
